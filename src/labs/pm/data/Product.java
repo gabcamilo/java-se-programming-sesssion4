8e6c77c3d5b4d369feca649d59029c4a0abfd82d
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-03-27 16:44:27 -0300

    Adding rating to Product

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -3,12 +3,29 @@ package labs.pm.data;
 import java.math.BigDecimal;
 
 import static java.math.RoundingMode.HALF_UP;
+import static labs.pm.data.Rating.*;
 
 public class Product {
     private int id;
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     private String name;
     private BigDecimal price;
+    private Rating rating;
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
+
+    public Product() {
+        this(0, "no name", BigDecimal.ZERO);
+    }
 
     public int getId() {
         return id;
@@ -36,5 +53,9 @@ public class Product {
 
     public BigDecimal getDiscount() {
         return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
     }
 }
