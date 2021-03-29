a2534a6d68ce1b3727c6488635d149760ef1608e
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-03-29 00:24:31 -0300

    Adding Food and Drink subclasses of Product

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -1,15 +1,15 @@
 package labs.pm.app;
 
-import labs.pm.data.Product;
-import labs.pm.data.Rating;
+import labs.pm.data.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Shop {
     public static void main(String... args) {
         Product p1 = new Product(101, "Tea", BigDecimal.valueOf(1.99));
-        Product p2 = new Product(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FIVE_STAR);
-        Product p3 = new Product(103, "Cake", BigDecimal.valueOf(3.99),Rating.FIVE_STAR);
+        Product p2 = new Drink(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FIVE_STAR);
+        Product p3 = new Food(103, "Cake", BigDecimal.valueOf(3.99),Rating.FIVE_STAR, LocalDate.now().plusDays(2));
         Product p4 = new Product();
         System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount() + " " + p1.getRating().getStars());
         System.out.println(p2.getId() + " " + p2.getName() + " " + p2.getPrice() + " " + p2.getDiscount() + " " + p2.getRating().getStars());
---
+++ src/labs/pm/data/Drink.java
@@ -0,0 +1,9 @@
+package labs.pm.data;
+
+import java.math.BigDecimal;
+
+public class Drink extends Product {
+    public Drink(int id, String name, BigDecimal price, Rating rating) {
+        super(id, name, price, rating);
+    }
+}
