93b1a86f6afc481b2588e44c2ff78d70585115f4
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-03-31 03:06:15 -0300

    Removing redundant modifiers from Rateable interface

--- src/labs/pm/data/Rateable.java
+++ src/labs/pm/data/Rateable.java
@@ -2,19 +2,19 @@ package labs.pm.data;
 
 @FunctionalInterface //this annotation allows only one abstract method
 public interface Rateable<T> {
-    public static final Rating DEFAULT_RATING = Rating.NOT_RATED;
+    Rating DEFAULT_RATING = Rating.NOT_RATED;
 
     T applyRating(Rating rating);
 
-    public default Rating getRating() {
+    default Rating getRating() {
         return DEFAULT_RATING;
     }
 
-    public default T applyRating(int stars) {
+    default T applyRating(int stars) {
         return applyRating(convert(stars));
     }
 
-    public static Rating convert (int stars) {
+    static Rating convert (int stars) {
         return (stars >= 0 && stars <=5) ? Rating.values()[stars] : DEFAULT_RATING;
     }
 
